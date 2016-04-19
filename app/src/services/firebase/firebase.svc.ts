07b0cccc8d072e792116e92c8f29ae491ad4188e
Joel Valencia <trekkerj1@gmail.com>
2016-04-19 11:53:19 -0500

    basic unauthenticated login and user creation

---
+++ app/src/services/firebase/firebase.svc.ts
@@ -0,0 +1,56 @@
+import {async, register} from 'platypus';
+import BaseService from '../base/base.svc';
+
+declare var Firebase: any;
+
+export default class FirebaseService extends BaseService {
+    
+    bandRegister(username:string) {
+        return new this.Promise((fulfill, reject) => {
+            try {
+                var requestsFirebase = new Firebase("https://songrequests.firebaseio.com");
+                var bandsFirebase = requestsFirebase.child("bands");
+                
+                var newUserRef = bandsFirebase.push({
+                    username: username
+                });
+                
+                var newUserKey = newUserRef.key();
+                
+                fulfill(newUserKey);
+
+            } catch (err) {
+                reject(err);
+            }
+        });
+
+    }
+
+    bandLogin(username:string) {
+        return new this.Promise((fulfill, reject) => {
+            try {
+                var requestsFirebase = new Firebase("https://songrequests.firebaseio.com");
+                var bandsFirebase = requestsFirebase.child("bands");
+
+                bandsFirebase.once("value", (snapshot: any) => {
+                    var allBands = snapshot.val();
+                    
+                    for (var key in allBands) {
+                        if (allBands[key].username == username) {
+                            fulfill(key);
+                        }
+                    }
+
+                }, (errorObject: any) => {
+                    console.log("The read failed: " + errorObject.code);
+                });
+
+            } catch (err) {
+                reject(err);
+            }
+        });
+
+    }
+}
+
+register.injectable('firebase-svc', FirebaseService);
--- app/src/viewcontrols/home/home.vc.ts
+++ app/src/viewcontrols/home/home.vc.ts
@@ -1,10 +1,30 @@
 import {register} from 'platypus';
 import BaseViewControl from '../base/base.vc';
+import FirebaseService from '../../services/firebase/firebase.svc';
 
 export default class HomeViewControl extends BaseViewControl {
     templateString: string = require('./home.vc.html');
 
-    context: any = {};
+    context: any = {
+        registerUsername: "",
+        loginUsername: ""
+    };
+    
+    constructor(private firebaseSvc:FirebaseService) {
+        super();
+    }
+    
+    bandRegister() {
+        this.firebaseSvc.bandRegister(this.context.registerUsername).then((result) => {
+            console.log("added user to database with key", result)
+        });
+    }
+    
+    bandLogin() {
+        this.firebaseSvc.bandLogin(this.context.loginUsername).then((result) => {
+            console.log("user found with key", result);
+        });
+    }
 }
 
-register.viewControl('home-vc', HomeViewControl);
+register.viewControl('home-vc', HomeViewControl, [FirebaseService]);
