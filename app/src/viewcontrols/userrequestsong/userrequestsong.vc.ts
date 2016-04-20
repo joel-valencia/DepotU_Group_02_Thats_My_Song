dd5816175e269b27b1c82a2ed25c45122ca69f23
treeplantedbystreams <davbros88@hotmail.com>
2016-04-20 13:56:24 -0500

    stuff

--- app/src/viewcontrols/userrequestsong/userrequestsong.vc.ts
+++ app/src/viewcontrols/userrequestsong/userrequestsong.vc.ts
@@ -5,6 +5,10 @@ export default class UserRequestSongViewControl extends BaseViewControl {
     templateString: string = require('./userrequestsong.vc.html');
 
     context: any = {};
+    
+    
 }
 
 register.viewControl('userrequestsong-vc', UserRequestSongViewControl);
+
+  
