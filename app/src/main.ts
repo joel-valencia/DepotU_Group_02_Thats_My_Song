d376534ffd13e3f17eab00260c64e2a1c77fe5b3
Joel Valencia <trekkerj1@gmail.com>
2016-04-26 15:42:18 -0500

    navbar menu

--- app/src/main.ts
+++ app/src/main.ts
@@ -2,3 +2,13 @@ import 'platypus';
 import 'platypusui';
 
 import './app/app';
+
+
+// open or close menu
+document.getElementById('menu-button').addEventListener('click', function() {
+    if (document.getElementById('menu').style.display =="none") {
+        document.getElementById('menu').style.display = "block";
+    } else {
+        document.getElementById('menu').style.display = "none";
+    }
+});
